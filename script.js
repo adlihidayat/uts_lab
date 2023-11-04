c8593a8215104e16645b4ffbb93f62ab546f4d8f
oneiric 30 <adlihidayat30@gmail.com>
2023-11-04 22:49:28 +0700

    updates a link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var typed = new Typed('#title', {
     strings: ['Seputar mengenai Presiden <span class="span">Indonesia</span>', 'Seputar mengenai Presiden <span class="span">Indonesia</span>'],
     typeSpeed: 100,
-    backSpeed : 50,
+    backSpeed : 0,
     loop: true,
 });
