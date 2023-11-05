9c0beac4d1f6bb9451c1bf44440a3296d38bac03
oneiric 30 <adlihidayat30@gmail.com>
2023-11-05 14:32:25 +0700

    changes v2 page 2

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var typed = new Typed('#title', {
     strings: ['Seputar mengenai Presiden <span class="span">Indonesia</span>', 'Seputar mengenai Presiden <span class="span">Indonesia</span>'],
-    typeSpeed: 100,
-    backSpeed : 0,
+    typeSpeed: 50,
+    backSpeed : 20,
     loop: true,
 });
+VanillaTilt.init(document.getElementById('batmeow'));   
